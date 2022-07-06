238a4bed7b8b1523cc0b8142ea5a0a38c08aee65
7990 <bianchijarron@gmail.com>
2022-07-06 11:54:39 +0800

    VS Priject

---
+++ WPF_Learning/WPF_Learning/simple_2.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_Learning
+{
+    /// <summary>
+    /// simple_2.xaml 的互動邏輯
+    /// </summary>
+    public partial class simple_2 : Window
+    {
+        public simple_2()
+        {
+            InitializeComponent();
+        }
+
+        int count = 0;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            count++;
+            label_01.Content = $"clicl {count} times";
+        }
+    }
+}
