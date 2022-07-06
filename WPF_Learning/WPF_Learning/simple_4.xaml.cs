cea1fa516520bddfdd7b094e6292dbc675b034e6
7990 <bianchijarron@gmail.com>
2022-07-06 14:25:27 +0800

    simple__half

---
+++ WPF_Learning/WPF_Learning/simple_4.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_Learning
+{
+    /// <summary>
+    /// simple_4.xaml 的互動邏輯
+    /// </summary>
+    public partial class simple_4 : Window
+    {
+        public simple_4()
+        {
+            InitializeComponent();
+            timer.Interval = new TimeSpan(10000);
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            p_bar.Value++;
+            if (p_bar.Value > 100)
+                timer.Stop();
+        }
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //p_bar.Value = 50;
+            timer.Start();
+        }
+
+        System.Windows.Threading.DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer();
+    }
+}
