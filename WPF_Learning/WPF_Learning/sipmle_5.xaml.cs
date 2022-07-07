a863a7b70ef14d579d98f0c11c62cfc1e20d4758
7990 <bianchijarron@gmail.com>
2022-07-07 08:58:28 +0800

    Lesson 1 Done

--- WPF_Learning/WPF_Learning/sipmle_5.xaml.cs
+++ WPF_Learning/WPF_Learning/sipmle_5.xaml.cs
@@ -19,15 +19,32 @@ namespace WPF_Learning
     /// </summary>
     public partial class sipmle_5 : Window
     {
+        System.Windows.Threading.DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer();
+
         public sipmle_5()
         {
             InitializeComponent();
+            timer.Interval = new TimeSpan(10000);
+            timer.Tick += Timer_Tick;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             //MessageBox.Show(datatimepicker1.SelectedDateTime.ToString());
             HandyControl.Controls.Dialog.Show(new dialog(datatimepicker1.SelectedDateTime.ToString()));
+        }
+
+        private void pbar_Checked(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            pbar.Progress++;
+            wpbar.Value++;
+            if (pbar.Progress > 100)
+                timer.Stop();
         }
     }
 }
